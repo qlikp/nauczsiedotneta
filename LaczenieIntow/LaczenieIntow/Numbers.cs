19f32f5d8ce7980a5e144cb7bb744fb57e75c56d
Paweł Kulik <qlikp0@gmail.com>
2020-06-03 09:05:28 +0200

    LaczenieIntow

---
+++ LaczenieIntow/LaczenieIntow/Numbers.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace LaczenieIntow
+{
+    class Numbers
+    {
+        List<string> numberList = new List<string>();
+        public int maxInt;
+        public int minInt;
+        public string errorText = "";
+        public Numbers(string[] args)
+        {
+
+            if (args.Length > 0)
+            {
+                foreach (var a in args)
+                {
+                    if (int.TryParse(a, out int outputVal))
+                    {
+                        numberList.Add(a);
+
+                    }
+
+                }
+            }
+
+            this.OrderingTool();
+
+        }
+
+        private void OrderingTool()
+        {
+            string maxN_str = "";
+            string minN_str = "";
+
+            foreach (var a in numberList.OrderByDescending(n => n))
+            {
+                maxN_str += a;
+
+            }
+
+            foreach (var a in numberList.OrderBy(n => n))
+            {
+                minN_str += a;
+
+            }
+
+            try
+            {
+
+                maxInt = Int32.Parse(maxN_str);
+                minInt = Int32.Parse(minN_str);
+            }
+            catch (FormatException)
+            {
+                errorText = "There is no 'Int' on input";
+            }
+            catch (OverflowException)
+            {
+                errorText = "Output number is too big to be an 'Int' type";
+            }
+
+
+        }
+
+
+    }
+}
---
+++ LaczenieIntow/LaczenieIntow/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaczenieIntow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            List<string> lista = new List<string>();
+
+            Numbers liczby = new Numbers(args);
+
+
+            Console.WriteLine($"Max: {liczby.maxInt}");
+            Console.WriteLine($"Min: {liczby.minInt}");
+            Console.ReadKey();
+
+        }
+    }
+}
